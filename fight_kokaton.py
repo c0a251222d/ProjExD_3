42be8f4306fc290bf29f93877d263e1a959cacaa
c0a251222d <c0a251222d@edu.teu.ac.jp>
2026-04-28 18:05:12 +0900

    元に戻した

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -165,6 +165,7 @@ def main():
     bird = Bird((300, 200))
     bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)]
     beams = []
+    # beam = None  # ゲーム初期化時にはビームは存在しない
     score = Score()
     clock = pg.time.Clock()
     tmr = 0
